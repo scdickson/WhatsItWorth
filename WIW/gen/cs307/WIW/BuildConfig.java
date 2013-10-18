a86cfd39cc440f54d65d081898d494ab8575bb09
nbachman <nbachman@purdue.edu>
2013-10-17 21:39:14 -0400

    Eclipse project added

---
+++ WIW/gen/cs307/WIW/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package cs307.WIW;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ WIW/src/cs307/WIW/NetworkActivity.java
@@ -0,0 +1,19 @@
+package cs307.WIW;
+import android.net.Uri;
+
+
+public class NetworkActivity implements Runnable {
+
+	private static String ServerIP = "98.226.145.27";
+	
+	public NetworkActivity(Uri image){
+		
+	}
+
+	@Override
+	public void run() {
+		
+
+	}
+
+}
