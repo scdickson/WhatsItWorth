374b9b0b66d0869f22558fcce1c4c99034018064
nbachman <nbachman@purdue.edu>
2013-11-10 16:27:02 -0500

    What's It Worth with Cloud
    Bastardized version of cloud recognition

---
+++ CloudRecognition-2-6-7/obj/local/armeabi-v7a/objs-debug/CloudReco/Texture.o.d
@@ -0,0 +1,67 @@
+obj/local/armeabi-v7a/objs-debug/CloudReco/Texture.o: jni/Texture.cpp \
+ jni/Texture.h \
+ /cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/jni.h \
+ /cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sys/cdefs.h \
+ /cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h \
+ /cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/android/api-level.h \
+ jni/SampleUtils.h \
+ /cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/stdio.h \
+ /cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sys/types.h \
+ /cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/stdint.h \
+ /cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sys/_types.h \
+ /cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/machine/_types.h \
+ /cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/linux/posix_types.h \
+ /cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/linux/stddef.h \
+ /cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/linux/compiler.h \
+ /cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/asm/posix_types.h \
+ /cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/asm/types.h \
+ /cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/linux/types.h \
+ /cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/machine/kernel.h \
+ /cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h \
+ /cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/android/log.h \
+ /cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/string.h \
+ /cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/malloc.h
+
+jni/Texture.h:
+
+/cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/jni.h:
+
+/cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sys/cdefs.h:
+
+/cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/android/api-level.h:
+
+jni/SampleUtils.h:
+
+/cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/stdio.h:
+
+/cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sys/types.h:
+
+/cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/stdint.h:
+
+/cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sys/_types.h:
+
+/cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/machine/_types.h:
+
+/cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/linux/posix_types.h:
+
+/cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/linux/stddef.h:
+
+/cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/linux/compiler.h:
+
+/cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/asm/posix_types.h:
+
+/cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/asm/types.h:
+
+/cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/linux/types.h:
+
+/cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/machine/kernel.h:
+
+/cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h:
+
+/cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/android/log.h:
+
+/cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/string.h:
+
+/cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/malloc.h:
---
+++ CloudRecognition-2-6-7/obj/local/armeabi/objs-debug/CloudReco/SampleMath.o.d
@@ -0,0 +1,105 @@
+obj/local/armeabi/objs-debug/CloudReco/SampleMath.o: jni/SampleMath.cpp \
+ jni/SampleMath.h jni/../../../build/include/QCAR/Tool.h \
+ jni/../../../build/include/QCAR/System.h \
+ /cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/stdio.h \
+ /cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sys/cdefs.h \
+ /cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h \
+ /cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/android/api-level.h \
+ /cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sys/types.h \
+ /cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/stdint.h \
+ /cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sys/_types.h \
+ /cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/machine/_types.h \
+ /cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/linux/posix_types.h \
+ /cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/linux/stddef.h \
+ /cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/linux/compiler.h \
+ /cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/asm/posix_types.h \
+ /cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/asm/types.h \
+ /cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/linux/types.h \
+ /cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/machine/kernel.h \
+ /cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h \
+ jni/../../../build/include/QCAR/Matrices.h \
+ jni/../../../build/include/QCAR/Vectors.h \
+ /cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/math.h \
+ /cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/limits.h \
+ /cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sys/limits.h \
+ /cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/linux/limits.h \
+ /cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/machine/internal_types.h \
+ /cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/machine/limits.h \
+ /cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sys/syslimits.h \
+ /cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/asm/page.h \
+ /cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/stdlib.h \
+ /cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/string.h \
+ /cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/malloc.h \
+ /cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/alloca.h \
+ /cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/strings.h \
+ /cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/memory.h
+
+jni/SampleMath.h:
+
+jni/../../../build/include/QCAR/Tool.h:
+
+jni/../../../build/include/QCAR/System.h:
+
+/cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/stdio.h:
+
+/cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sys/cdefs.h:
+
+/cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/android/api-level.h:
+
+/cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sys/types.h:
+
+/cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/stdint.h:
+
+/cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sys/_types.h:
+
+/cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/machine/_types.h:
+
+/cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/linux/posix_types.h:
+
+/cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/linux/stddef.h:
+
+/cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/linux/compiler.h:
+
+/cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/asm/posix_types.h:
+
+/cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/asm/types.h:
+
+/cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/linux/types.h:
+
+/cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/machine/kernel.h:
+
+/cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h:
+
+jni/../../../build/include/QCAR/Matrices.h:
+
+jni/../../../build/include/QCAR/Vectors.h:
+
+/cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/math.h:
+
+/cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/limits.h:
+
+/cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sys/limits.h:
+
+/cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/linux/limits.h:
+
+/cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/machine/internal_types.h:
+
+/cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/machine/limits.h:
+
+/cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sys/syslimits.h:
+
+/cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/asm/page.h:
+
+/cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/stdlib.h:
+
+/cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/string.h:
+
+/cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/malloc.h:
+
+/cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/alloca.h:
+
+/cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/strings.h:
+
+/cygdrive/c/Users/Nix/Development/adt-bundle-windows-x86_64-20131030/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/memory.h:
